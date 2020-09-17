1bb4b256cfe5ddadee79a9393bbb7abb81516a1f
OOJohn <oojohn1981@gmail.com>
2020-09-18 00:52:24 +0800

    initial commit

--- app/src/main/java/com/oojohn/guss/MainActivity.kt
+++ app/src/main/java/com/oojohn/guss/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.oojohn.guss
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
